fe906ea5ad28007e45b24e1e4ac222b29a5fc328
Joe <k4nuck@yahoo.com>
2018-02-11 22:18:28 -0500

    Additional Smart Classes

---
+++ smartcontroller.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+class SmartController:
+	
+	def __init__ (self):
+		print "JB - SmartController Start"
---
+++ smartdevice.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+class SmartDevice:
+	
+	def __init__ (self):
+		print "JB - SmartDevice Start"
